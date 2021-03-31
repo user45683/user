0a7b2128d04f7f95e104d369974328874e690d5b
Your Name <you@example.com>
2021-03-31 10:23:14 +0300

    add

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <!doctype html><html lang="ru"><head>
-
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>user</title>
 <link rel="manifest" href="manifest.json">
+<script src="js/main.js"></script>
 <head><body>
-
-
-<script src="js/serwice-worker.js"></script>
+<button class="add-button">download</button>
 </body></html>
--- js/service-worker.js
+++ js/service-worker.js
@@ -3,7 +3,7 @@
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('pwa').then((cache) => cache.addAll([
+    caches.open('user').then((cache) => cache.addAll([
   '/user',
   '/user/index.php',
   '/user/css/style.css',
@@ -17,29 +17,4 @@ self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request)),
   );
-});
-
-
-// Installing Service Worker
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-  e.waitUntil((async () => {
-    const cache = await caches.open(cacheName);
-    console.log('[Service Worker] Caching all: app shell and content');
-    await cache.addAll(contentToCache);
-  })());
-});
-
-// Fetching content using Service Worker
-self.addEventListener('fetch', (e) => {
-  e.respondWith((async () => {
-    const r = await caches.match(e.request);
-    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-    if (r) return r;
-    const response = await fetch(e.request);
-    const cache = await caches.open(cacheName);
-    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-    cache.put(e.request, response.clone());
-    return response;
-  })());
 });
