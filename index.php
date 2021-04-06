1c1224222f13414ce760b3a8998f67ef40760a53
Your Name <you@example.com>
2021-04-06 15:42:22 +0300

    add

--- index.php
+++
@@ -1,9 +0,0 @@
-<!doctype html><html lang="ru"><head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>user</title>
-<link rel="manifest" href="manifest.json">
-<script src="js/main.js"></script>
-<head><body>
-<button class="add-button">download</button>
-</body></html>
