426fe1c91d1a354b897b40d0ee77c2d254f657db
Your Name <you@example.com>
2021-07-04 00:55:49 +0300

    add

--- store/a1/node/app.js
+++ store/a1/node/app.js
@@ -19,6 +19,12 @@ io.on('connection', (socket) => {
   });
 });
 
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log('message: ', msg);
+  });
+});
+
 server.listen(3000, () => {
-  console.log('localhost:3000');
+  console.log('listening on *:3000');
 });
