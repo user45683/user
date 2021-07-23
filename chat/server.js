7766a5354af2eef3b8ca385d46fa9e9488b8fd5f
Your Name <you@example.com>
2021-07-23 22:26:32 +0300

    add

--- chat/server.js
+++ chat/server.js
@@ -1,21 +1,22 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
+
+
+
+
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
   });
 });
 
-
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
