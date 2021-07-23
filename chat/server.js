97afaa050745f16ca533ba26ceb2ec54d51882ed
Your Name <you@example.com>
2021-07-23 19:03:23 +0300

    add

--- chat/server.js
+++ chat/server.js
@@ -4,8 +4,17 @@ const http = require('http');
 const server = http.createServer(app);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
